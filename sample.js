4aa4a96491ad2815d0e52f4c70d0d180a0ea5044
chandini2007 <2400032767@kluniversity.in>
2025-11-28 10:54:52 +0530

    Intialize commit

---
+++ sample.js
@@ -0,0 +1,16 @@
+
+function greetUser(name) {
+    return `Hello, ${name}! Welcome to GitHub testing.`;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+const user = "Chandini";
+console.log(greetUser(user));
+
+const result = multiply(7, 5);
+console.log("Multiplication Result:", result);
+
+console.log("If you're seeing this, your push to GitHub worked!");
